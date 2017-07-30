2760dee7b3339ec66b8e032b948b093fbd9e43ca
develephant <code@develephant.com>
2017-07-30 17:30:32 -0500

    clean

--- lua/plugin/mailgun-sender.lua
+++ lua/plugin/mailgun-sender.lua
@@ -6,8 +6,20 @@ local Library = require "CoronaLibrary"
 
 local lib = Library:new{ name='mailgun-sender', publisherId='com.develephant' }
 
-local mime = require("mime")
-local b64 = mime.encode("base64")
+local b64_enc = function(data)
+  -- character table string
+  local b='ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/'
+  return ((data:gsub('.', function(x) 
+      local r,b='',x:byte()
+      for i=8,1,-1 do r=r..(b%2^i-b%2^(i-1)>0 and '1' or '0') end
+      return r;
+  end)..'0000'):gsub('%d%d%d?%d?%d?%d?', function(x)
+      if (#x < 6) then return '' end
+      local c=0
+      for i=1,6 do c=c+(x:sub(i,i)=='1' and 2^(6-i) or 0) end
+      return b:sub(c+1,c+1)
+  end)..({ '', '==', '=' })[#data%3+1])
+end
 
 local api = {
   key = nil,
@@ -110,7 +122,7 @@ function api:send( listener )
 
   local headers = {
     ["Content-Type"] = "application/x-www-form-urlencoded",
-    ["Authentication"] = b64("api:"..self.key)
+    ["Authorization"] = "Basic "..tostring(b64_enc("api:"..self.key))
   }
 
   local form = {}
@@ -133,12 +145,12 @@ function api:send( listener )
   return network.request(url, "POST", listener, params)
 end
 
-function lib.new( key, domain )
+function lib.new( key, domain, debug )
 
   assert(key, "An API key value is required to send a message.")
   assert(domain, "A domain value is required to send a message.")
 
-  local msgObj = setmetatable({key=key, domain=domain}, {__index=api})
+  local msgObj = setmetatable({key=key, domain=domain, debug=debug}, {__index=api})
 
   return msgObj
 end
